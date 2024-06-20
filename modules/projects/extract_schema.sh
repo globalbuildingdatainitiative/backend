202ddb12560664fa6242a84fc2c677be705ff8ed
Christian Kongsgaard <kongsgaard.c@gmail.com>
2024-06-20 09:07:23 +0200

    updated extract schema files

--- modules/projects/extract_schema.sh
+++ modules/projects/extract_schema.sh
@@ -1,7 +1,15 @@
 #! /usr/bin/bash
 set -e
 
+BASEDIR=$(dirname $0)
+NAMESPACE=projects
+ROUTER_SCHEMA_DIR=$(dirname $BASEDIR)/router/schemas/$NAMESPACE
+
 echo "Running Post-Sync Copy"
-for podname in $(kubectl -n projects get pods -l app=backend -o json| jq -r '.items[].metadata.name'); do
-  kubectl cp projects/"${podname}":/app/graphql/schema.graphql graphql/schema.graphql;
+for podname in $(kubectl -n $NAMESPACE get pods -l app=backend -o json| jq -r '.items[].metadata.name'); do
+  kubectl cp $NAMESPACE/"${podname}":/app/graphql/schema.graphql $BASEDIR/graphql/schema.graphql;
+  if [[ -d "$ROUTER_SCHEMA_DIR" ]]
+  then
+    kubectl cp $NAMESPACE/"${podname}":/app/graphql/schema.graphql $ROUTER_SCHEMA_DIR/schema.graphql;
+  fi
 done
