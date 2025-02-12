7a1baae6d4f80c8b89de56c01fb12a382b8b6951
Christian Kongsgaard <kongsgaard.c@gmail.com>
2025-02-12 14:34:22 +0100

    Chrk/243 upload log (#109)
    * wip
    
    * improved openbdf schema
    
    * removed comments
    
    * updated dependencies

---
+++ modules/projects/tests/unit/logic/test_openbdf.py
@@ -0,0 +1,12 @@
+import pytest
+from jsonschema import Draft202012Validator
+
+from logic import serialize_openbdf_schema
+
+
+@pytest.mark.asyncio
+async def test_serialize_openbdf_schema():
+    schema = await serialize_openbdf_schema()
+
+    assert schema
+    Draft202012Validator.check_schema(schema)
